dd33a19196f27fdb58f88ab3f295882c2e61ff19
Liam Ryan <ldryan0@blc00.bl.pvt>
2021-12-02 15:43:54 +0800

    matAdd and matHad now MxN

--- matAdd/matAdd.cu
+++ matAdd/matAdd.cu
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
-#define N 8 //number of rows
+#define M 6 // number of rows
+#define N 8 // number of columns
 
 #define numThread 16
 #define numBlock 4
 
-__global__ void matAdd(float *A, float *B, float *C) {
+__global__ void matHad(float *A, float *B, float *C) {
     int x = blockIdx.x*blockDim.x + threadIdx.x;
     int y = blockIdx.y*blockDim.y + threadIdx.y;
     int idx = x + y*N;
@@ -16,7 +17,7 @@ __global__ void matAdd(float *A, float *B, float *C) {
 }
 
 void print2DArray(float *arr) {
-    for(int i=0; i < N*N; i++) {
+    for(int i=0; i < M*N; i++) {
         if ((i % N == 0) && (i != 0)) {
             printf("\n");
         }
@@ -31,33 +32,33 @@ int main(int argc, char* argv[]) {
     float *da, *db, *dc; // device (GPU) arrays
 
     printf("Creating host and device arrays...\n\n");
-    ha = (float*)malloc(N*N*sizeof(float));
-    hb = (float*)malloc(N*N*sizeof(float));
-    hc = (float*)malloc(N*N*sizeof(float));
+    ha = (float*)malloc(M*N*sizeof(float));
+    hb = (float*)malloc(M*N*sizeof(float));
+    hc = (float*)malloc(M*N*sizeof(float));
 
-    for (int i=0; i < N*N; i++) {
+    for (int i=0; i < M*N; i++) {
         ha[i] = 1.0*i;
         hb[i] = 2.0*i;
     }
 
-    cudaMalloc((void**)&da, N*N*sizeof(float));
-    cudaMalloc((void**)&db, N*N*sizeof(float));
-    cudaMalloc((void**)&dc, N*N*sizeof(float));
+    cudaMalloc((void**)&da, M*N*sizeof(float));
+    cudaMalloc((void**)&db, M*N*sizeof(float));
+    cudaMalloc((void**)&dc, M*N*sizeof(float));
 
     // copy host arrays over to device
-    cudaMemcpy(da, ha, N*N*sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(db, hb, N*N*sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(da, ha, M*N*sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(db, hb, M*N*sizeof(float), cudaMemcpyHostToDevice);
 
     // output input matrices
     print2DArray(ha);
     print2DArray(hb);
     
-    printf("Performing elementwise addition...\n\n");
+    printf("Performing elementwise multiplication...\n\n");
     printf("Thread sequence: ");
     // perform parralised addition of matrics
-    matAdd<<<numBlock, numThread>>>(da, db, dc);
+    matHad<<<numBlock, numThread>>>(da, db, dc);
 
-    cudaMemcpy(hc, dc, N*N*sizeof(float), cudaMemcpyDeviceToHost);
+    cudaMemcpy(hc, dc, M*N*sizeof(float), cudaMemcpyDeviceToHost);
     printf("\n\n");
 
     // output resultant matrix
@@ -67,8 +68,8 @@ int main(int argc, char* argv[]) {
     // absolute error 1e-5
     bool success = true;
     int total=0;
-    printf("Checking %d values in the array...\n\n", N*N);
-    for (int i=0; i<N*N; i++) {
+    printf("Checking %d values in the array...\n\n", M*N);
+    for (int i=0; i<M*N; i++) {
         if (abs((ha[i] + hb[i]) - hc[i]) > 1e-5) {
             printf( "Error:  %0.2e + %0.2e != %0.2e\n", ha[i], hb[i], hc[i] );
             success = false;
@@ -77,7 +78,7 @@ int main(int argc, char* argv[]) {
         }
     }
     if (success) {
-        printf("%d out of %d values correct!\n", total, N*N);
+        printf("%d out of %d values correct!\n", total, M*N);
     }
 
     // free host arrays

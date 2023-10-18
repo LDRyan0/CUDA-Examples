60bafb88ebe395d2814b1b00881c77732623fa54
Liam Ryan <liamdryan01@gmail.com>
2023-10-18 18:19:30 +0800

    fix units in stats.cu

--- src/stats.cu
+++ src/stats.cu
@@ -7,8 +7,8 @@ void showProperties(int devId) {
         cudaGetDeviceProperties(&prop, devId);
 
         std::cout << "Name: " << prop.name << "\n";
-        std::cout << "totalGlobalMem: " << prop.totalGlobalMem / (1024 * 1024 * 1024) << "GB\n";
-        std::cout << "sharedMemPerBlock: " << prop.sharedMemPerBlock / 1024 << "MB\n";
+        std::printf("totalGlobalMem: %.1f GB\n", prop.totalGlobalMem / (1024.0 * 1024.0 * 1024.0));
+        std::printf("sharedMemPerBlock: %.1f MB\n", prop.sharedMemPerBlock / 1024.0);
         std::cout << "regsPerBlock: " << prop.regsPerBlock << "\n";
         std::cout << "warpSize: " << prop.warpSize << "\n";
         std::cout << "maxThreadsPerBlock: " << prop.maxThreadsPerBlock << "\n";
@@ -16,18 +16,18 @@ void showProperties(int devId) {
             std::printf("maxThreadsDim[%d]: %d\n", i, prop.maxThreadsDim[i]);
         for(int i = 0; i < 3; ++i)
             std::printf("maxGridSize[%d]: %d\n", i, prop.maxGridSize[i]);
-        std::cout << "clockRate: " << prop.clockRate << "\n";
+        std::printf("clockRate: %.1f GHz\n", prop.clockRate / (1024.0*1024.0));
         std::cout << "totalConstMem: " << prop.totalConstMem << "\n";
         std::cout << "capability: " << prop.major  << "." << prop.minor << "\n";
-        std::cout << "multiProcessorCount " << prop.multiProcessorCount << "\n";
+        std::cout << "multiProcessorCount: " << prop.multiProcessorCount << "\n";
         std::cout << "integrated: " << (prop.integrated ? "TRUE" : "FALSE") << "\n";
         std::cout << "concurrentKernels: " << (prop.concurrentKernels ? "TRUE" : "FALSE") << "\n";
-        std::cout << "memoryClockRate:" << prop.memoryClockRate << "\n";
-        std::cout << "l2CacheSize:" << prop.l2CacheSize << "\n";
+        std::printf("memoryClockRate: %.1f GHz\n", prop.memoryClockRate / (1024.0*1024.0));
+        std::printf("l2CacheSize: %.1f MB\n", prop.l2CacheSize / (1024.0 * 1024.0));
         std::cout << "maxThreadsPerMultiProcessor: " << prop.maxThreadsPerMultiProcessor << "\n";
         std::cout << "globalL1CacheSupported: " << (prop.globalL1CacheSupported ? "TRUE" : "FALSE") << "\n";
         std::cout << "localL1CacheSupported: " << (prop.localL1CacheSupported ? "TRUE" : "FALSE")<< "\n";
-        std::cout << "sharedMemPerMultiprocessor: " << prop.sharedMemPerMultiprocessor << "\n";
+        std::printf("sharedMemPerMultiprocessor: %.1f kB\n", prop.sharedMemPerMultiprocessor / 1024.0);
         std::cout << "regsPerMultiprocessor: " << prop.sharedMemPerMultiprocessor << "\n";
 }
 
